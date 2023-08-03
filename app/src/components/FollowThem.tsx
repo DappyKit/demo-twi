87ce9c546696f8b89bf965b59ac1470926f88f85
igar1991 <iiggaarr19912004@gmail.com>
2023-08-03 10:18:14 +0300

    feat: upgraded follow all with images

--- app/src/components/FollowThem.tsx
+++ app/src/components/FollowThem.tsx
@@ -7,7 +7,21 @@ const FollowThem: React.FC = () => {
                 <h3 className="fs-3">Follow them all</h3>
             </div>
 
-            AVATARS HERE
+            <div className="row">
+                {[...Array(9)].map((_, i) => (
+                    <div className="col-4 my-2 d-flex justify-content-center" key={i}>
+                        <div className="bg-secondary rounded-circle d-flex align-items-center justify-content-center" style={{width: '100%', paddingTop: '100%', position: 'relative'}}>
+                            <img
+                                src={`https://source.unsplash.com/random/150x150?sig=${i}`}
+                                className="img-fluid rounded-circle position-absolute top-0 start-0"
+                                alt="Avatar"
+                                style={{objectFit: 'cover'}}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <button className="btn btn-outline-success btn-lg w-100 mt-2">Follow</button>
         </div>
     )
 }
--- app/src/components/RightSidebar.tsx
+++ app/src/components/RightSidebar.tsx
@@ -3,7 +3,7 @@ import FollowThem from './FollowThem'
 
 const Feed: React.FC = () => {
     return (
-        <div className="col-lg-4 mt-4 d-none d-lg-block">
+        <div className="col-lg-3 mt-4 d-none d-lg-block">
             <div className="position-sticky" style={{top: 0}}>
                 <FollowThem/>
             </div>
